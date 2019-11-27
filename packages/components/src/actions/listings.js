799204bf7af08074b7b69149841c5dd224785ad9
phattv <phattv.dev@gmail.com>
2019-11-28 02:34:03 +0800

    Add get listings/:id action & reducer

--- packages/components/src/actions/listings.js
+++ packages/components/src/actions/listings.js
@@ -3,6 +3,9 @@ import {
   GET_LISTINGS_REQUEST,
   GET_LISTINGS_SUCCESS,
   GET_LISTINGS_FAILURE,
+  GET_LISTING_REQUEST,
+  GET_LISTING_SUCCESS,
+  GET_LISTING_FAILURE,
 } from './index';
 import { BASE_URL } from '../constants';
 
@@ -24,6 +27,30 @@ export const getListings = () => {
       .catch(error =>
         dispatch({
           type: GET_LISTINGS_FAILURE,
+          payload: error.response,
+        }),
+      );
+  };
+};
+
+export const getListing = id => {
+  return dispatch => {
+    dispatch({
+      type: GET_LISTING_REQUEST,
+      payload: { id },
+    });
+
+    axios
+      .get(`${BASE_URL}/listings/${id}`)
+      .then(response =>
+        dispatch({
+          type: GET_LISTING_SUCCESS,
+          payload: response.data,
+        }),
+      )
+      .catch(error =>
+        dispatch({
+          type: GET_LISTING_FAILURE,
           payload: error.response,
         }),
       );
--- packages/components/src/reducers/listings.js
+++ packages/components/src/reducers/listings.js
@@ -2,20 +2,22 @@ import {
   GET_LISTINGS_REQUEST,
   GET_LISTINGS_SUCCESS,
   GET_LISTINGS_FAILURE,
+  GET_LISTING_REQUEST,
+  GET_LISTING_SUCCESS,
+  GET_LISTING_FAILURE,
 } from '../actions';
-
-import { listings } from '../api/listings';
 
 const initialState = {
   loading: false,
   error: null,
-  list: listings,
+  list: [],
   details: {},
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_LISTINGS_REQUEST:
+    case GET_LISTING_REQUEST:
       return {
         ...state,
         isLoading: true,
@@ -27,7 +29,15 @@ export default function(state = initialState, action) {
         error: {},
         list: action.payload,
       };
+    case GET_LISTING_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        error: {},
+        details: action.payload,
+      };
     case GET_LISTINGS_FAILURE:
+    case GET_LISTING_FAILURE:
       return {
         ...state,
         isLoading: false,
